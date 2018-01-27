946a7fba7d165e43f66e0f1f8ebe3f6841c96b40
pedro <pedro@DESKTOP-APMJSKK>
2018-01-27 21:56:49 -0200

    Descagando a cena de Spawn

--- Assets/AimIKSolver.cs
+++ Assets/AimIKSolver.cs
@@ -18,19 +18,21 @@ public class AimIKSolver : MonoBehaviour {
         bodyTransform.rotation = Quaternion.LookRotation(bodyTransform.position + lookPos, Vector3.up);
         bodyTransform.Rotate(0f, 90f, 270f);
 
-        lookPos = target.position;
+        //lookPos = target.position;
+
+        //lookPos.x = leftWeaponTransform.position.x;
+        //lookPos.z = (leftWeaponTransform.position + leftWeaponTransform.forward) .z;
+
+        //leftWeaponTransform.rotation = Quaternion.LookRotation(leftWeaponTransform.position + lookPos, Vector3.up);
+        //leftWeaponTransform.Rotate(0f, 90f, 0f);
+
+        //lookPos = target.position;
 
         //lookPos.x = rightWeaponTransform.position.x;
-        //lookPos.z = rightWeaponTransform.position.z;
+        //lookPos.z = (rightWeaponTransform.position + rightWeaponTransform.forward).z;
 
         //rightWeaponTransform.rotation = Quaternion.LookRotation(rightWeaponTransform.position + lookPos, Vector3.down);
-
-        lookPos.x = leftWeaponTransform.position.x;
-        lookPos.z = leftWeaponTransform.position.z;
-
-
-        leftWeaponTransform.rotation = Quaternion.LookRotation(leftWeaponTransform.position + lookPos, Vector3.right);
-        leftWeaponTransform.Rotate(0f, 90f, -90f);
+        //rightWeaponTransform.Rotate(0f, 90f, 0f);
 
     }
 }
